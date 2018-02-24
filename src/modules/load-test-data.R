f2059d6a90e1eac182a81eb232a3ce0527d200d9
Thomas Skowronek <tc.skowronek@gmail.com>
2018-02-23 21:12:59 -0700

    Extracted out the code into a resuable script to load the test data.

---
+++ src/modules/load-test-data.R
@@ -0,0 +1,22 @@
+# Script to extract out the loading of the test data set with the purpose of being sourced into the notebooks
+
+# Load the cleansed training data
+test.values <- read.csv("../data/clean-test-set-values.csv", header = TRUE, na.strings = "NA")
+
+test.values$funder <- as.character(test.values$funder)
+test.values$funder[is.na(test.values$funder)] <- "UNKNOWN"
+test.values$funder <- as.factor(test.values$funder)
+
+test.values$permit <- as.character(test.values$permit)
+test.values$permit[is.na(test.values$permit)] <- "UNKNOWN"
+test.values$permit <- as.factor(test.values$permit)
+
+test.values$construction_year[is.na(test.values$construction_year)] <- 0
+
+test.values$population[is.na(test.values$population)] <- 0
+
+test.values$scheme_management <- as.character(test.values$scheme_management)
+test.values$scheme_management[is.na(test.values$scheme_management)] <- "UNKNOWN"
+test.values$scheme_management <- as.factor(test.values$scheme_management)
+
+test.values$wpt_name <- as.character(test.values$wpt_name)
